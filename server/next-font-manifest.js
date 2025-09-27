c2f62e6e03df4ff991d047d51f45fba3cb6f5e2a
Aditya Arya <aryaadityacr7@gmail.com>
2025-09-28 04:50:45 +0530

    Updates

---
+++ server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/adityaarya/Desktop/Desktop/Projects/CodeSathi/codesathi-comingsoon/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
